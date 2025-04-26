3910d73f0209d6b4c7665b5bcca20789653fb258
rajan vishwakarma <rajan31vishwakarma@gmail.com>
2025-04-26 09:36:37 +0530

    react logo to propelleraero

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
+import myLogo from './assets/myL/myLogo.png'; // fixed: consistent name
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={myLogo} className="logo" alt="My logo" /> {/* fixed: matches import name */}
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn propelleraero
         </a>
       </header>
     </div>
